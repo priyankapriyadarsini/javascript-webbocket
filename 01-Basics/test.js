1ec5ecdf6d5f34671db70bde64bfbfdc0afb91ee
priyankapriyadarsini <152278115+priyankapriyadarsini@users.noreply.github.com>
2023-11-28 11:59:41 +0000

    test file added

---
+++ 01-Basics/test.js
@@ -0,0 +1,8 @@
+console.log("Hello World");
+/*javascript is high level scripting language used to develop web & mobile  application efficiently.
+we used javascript in client side as well as server side programming language.
+javascript was developed to write logic/functionality to our code to perform everywhere.
+javascript was developed in 1997 and the 1st version was ES1 and later it was ES2,ES3.....
+Now we used ES6 version after  2015.
+ES6-Echma Script 6(Vanilla javascript)
+*/
